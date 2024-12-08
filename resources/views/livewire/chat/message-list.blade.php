1d76f7489c26629af9c901db6fc4e4bbae3ef1c0
Pablo Hernandez Villegas <38545507+Pblo16@users.noreply.github.com>
2024-12-08 01:13:28 -0600

    Adjust message list container height for improved visibility

--- resources/views/livewire/chat/message-list.blade.php
+++ resources/views/livewire/chat/message-list.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="flex-1 overflow-y-auto p-4 flex flex-col-reverse messages-container min-h-[calc(100dvh-230px)] max-h-[calc(100dvh-230px)]"
+    <div class="flex-1 overflow-y-auto p-4 flex flex-col-reverse messages-container min-h-[calc(100dvh-215px)] max-h-[calc(100dvh-215px)]"
         id="messages-container">
         @if($messages && $messages->count() > 0)
         <div class="flex flex-col-reverse gap-4">
