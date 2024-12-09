beb1f8780166dfb9012289ad0561c7703d1f0459
Pablo Hernandez Villegas <38545507+Pblo16@users.noreply.github.com>
2024-12-09 17:14:45 -0600

    Update asset file reference in manifest.json; configure Echo with additional WebSocket settings

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,10 +6,14 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER, // Make sure the cluster is set
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    wsHost: import.meta.env.VITE_PUSHER_HOST?.replace('https://', ''),
+    wsPort: import.meta.env.VITE_PUSHER_PORT,
+    wssPort: import.meta.env.VITE_PUSHER_PORT,
     forceTLS: true,
     encrypted: true,
     enabledTransports: ['ws', 'wss'],
+    disableStats: true
 });
 
 console.log(window.Echo.options);
