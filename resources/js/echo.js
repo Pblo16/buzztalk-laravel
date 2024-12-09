637931f521d84e73c13d327ff1955a1e1391fb5d
Pablo Hernandez Villegas <38545507+Pblo16@users.noreply.github.com>
2024-12-09 17:29:29 -0600

    Update Echo configuration to include environment variables for Pusher settings

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,7 +6,7 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER, // Make sure the cluster is set
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
     encrypted: true,
     enabledTransports: ['ws', 'wss'],
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    define: {
+        'process.env.VITE_PUSHER_APP_KEY': JSON.stringify(process.env.VITE_PUSHER_APP_KEY),
+        'process.env.VITE_PUSHER_APP_CLUSTER': JSON.stringify(process.env.VITE_PUSHER_APP_CLUSTER),
+        'process.env.VITE_PUSHER_SCHEME': JSON.stringify(process.env.VITE_PUSHER_SCHEME || 'https'),
+        'process.env.VITE_PUSHER_HOST': JSON.stringify(process.env.VITE_PUSHER_HOST),
+        'process.env.VITE_PUSHER_PORT': JSON.stringify(process.env.VITE_PUSHER_PORT || 443),
+    }
 });
