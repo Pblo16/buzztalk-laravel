fade7f36f2d077e07d3d067008047f5216611463
Pablo Hernandez Villegas <38545507+Pblo16@users.noreply.github.com>
2024-12-09 20:25:29 -0600

    Update asset file reference in manifest.json; remove Pusher import from bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,8 +3,5 @@ import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// Import Echo after window.Pusher is defined
-import Pusher from 'pusher-js';
-window.Pusher = Pusher;
 
 import './echo';
