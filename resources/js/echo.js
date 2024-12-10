24b023c797c64aa67046f63403a8694251274b56
Pablo Hernandez Villegas <38545507+Pblo16@users.noreply.github.com>
2024-12-09 21:08:48 -0600

    Update asset file reference in manifest.json; refactor Echo configuration to remove unnecessary options

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,18 +3,13 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
-const options = {
+window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
     encrypted: true,
     enabledTransports: ['ws', 'wss'],
-};
+});
 
-window.Echo = new Echo(options);
-
-// Optional: remove this in production
-if (import.meta.env.DEV) {
-    console.log('Echo options:', options);
-}
+console.log(window.Echo.options);
