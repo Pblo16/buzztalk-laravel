ef94d16659a008aad3ee915ce7625d63bd7d3ca8
Pablo Hernandez Villegas <38545507+Pblo16@users.noreply.github.com>
2024-12-10 02:54:05 -0600

    xd

--- resources/views/livewire/ui/navigation.blade.php
+++ resources/views/livewire/ui/navigation.blade.php
@@ -98,58 +98,28 @@
 
                         </li>
                         @else
-                        @if (Route::has('login'))
-                        <div class="space-x-4">
-                            @auth
-                            <a href="{{ url('/dashboard') }}"
-                                class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
-                            @else
-                            <a href="{{ route('login') }}"
-                                class="transition-colors bg-gray-200 text-black hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium dark:text-white dark:bg-[#252525] dark:hover:bg-white dark:hover:text-black">Log
-                                in</a>
 
-                            @endauth
-                        </div>
-                        @endif
                         @endauth
                     </ul>
                 </nav>
+
             </div>
+            @auth
+            @if (Route::has('login'))
+            <div class="space-x-4">
+                @auth
+                <a href="{{ url('/dashboard') }}"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
+                @else
+                <a href="{{ route('login') }}"
+                    class="transition-colors bg-gray-200 text-black hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium dark:text-white dark:bg-[#252525] dark:hover:bg-white dark:hover:text-black">Log
+                    in</a>
+
+                @endauth
+            </div>
+            @endif
+            @endauth
         </div>
-        <!-- Theme Switcher -->
-        <div class="absolute bottom-0 w-full p-4">
-            <button type="button"
-                class="hs-dark-mode-active:hidden block w-full hs-dark-mode group items-center text-gray-600 hover:text-blue-600 font-medium dark:text-gray-400 dark:hover:text-gray-500"
-                data-hs-theme-click-value="dark">
-                <span class="group inline-flex items-center gap-x-2">
-                    <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
-                    </svg>
-                </span>
-            </button>
-            <button type="button"
-                class="hs-dark-mode-active:block hidden w-full hs-dark-mode group items-center text-gray-600 hover:text-blue-600 font-medium dark:text-gray-400 dark:hover:text-gray-500"
-                data-hs-theme-click-value="light">
-                <span class="group inline-flex items-center gap-x-2">
-                    <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <circle cx="12" cy="12" r="4" />
-                        <path d="M12 2v2" />
-                        <path d="M12 20v2" />
-                        <path d="m4.93 4.93 1.41 1.41" />
-                        <path d="m17.66 17.66 1.41 1.41" />
-                        <path d="M2 12h2" />
-                        <path d="M20 12h2" />
-                        <path d="m6.34 17.66-1.41 1.41" />
-                        <path d="m19.07 4.93-1.41 1.41" />
-                    </svg>
-                </span>
-            </button>
-        </div>
-        <!-- End Theme Switcher -->
     </div>
     <!-- End Sidebar -->
 </div>
